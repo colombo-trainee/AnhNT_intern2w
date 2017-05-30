bab5fc606956c13bb5d238cdd479e772cfb702f6
hoang <zent.testphp@gmail.com>
2017-05-30 23:58:43 +0700

    test choi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,11 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">It</a>
+                    <a href="https://laracasts.com">is</a>
+                    <a href="https://laravel-news.com">Hoàng</a>
+                    <a href="https://forge.laravel.com">Babe</a>
+                    <a href="https://github.com/laravel/laravel">!!!!!!!!</a>
                 </div>
             </div>
         </div>
